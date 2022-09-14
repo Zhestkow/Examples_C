a9ddb519fdc78de91b84c7dbf225979e59e9c690
Yury <Zhestkow@gmail.com>
2022-09-14 18:46:41 +0300

    hjfjhasx

--- Example_002/Program.cs
+++ Example_002/Program.cs
@@ -1,10 +1,1 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.Write("Введите ваше имя ");
-        string username = Console.ReadLine();
-        Console.Write("Привет, ");
-        Console.Write(username);
-    }
-}
+﻿
