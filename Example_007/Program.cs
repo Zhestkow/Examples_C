c185b8098a897797bbf50f0d60079ef8f2258c60
Yury <Zhestkow@gmail.com>
2022-09-14 20:14:34 +0300

    example_007

---
+++ Example_007/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int xa = 12, ya = 12;
+int xb = 12, yb = 30;
+int xc = 60, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
