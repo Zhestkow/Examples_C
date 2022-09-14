3b02bbee697cdbafcce9a7eab6d456ef2173e7a1
Yury <Zhestkow@gmail.com>
2022-09-14 20:36:10 +0300

    Example_007 added

--- Example_007/Program.cs
+++ Example_007/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.Clear();
-int xa = 12, ya = 12;
-int xb = 12, yb = 30;
-int xc = 60, yc = 30;
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 30, ya = 1,
+    xb = 1, yb = 20,
+    xc = 60, yc = 20;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -11,3 +14,34 @@ Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
+
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3); // [0;3) 0 1 2
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
