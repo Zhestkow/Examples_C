4efca07eea880c0aae652c8a16d2311931d8f4dc
Yury <Zhestkow@gmail.com>
2022-09-14 19:51:49 +0300

    Add example_006

---
+++ Example_006/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberA = new Random().Next(1,100);
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,100);
+Console.WriteLine (numberB);
+int numberC = new Random().Next(1,100);
+Console.WriteLine (numberC);
+int numberD = new Random().Next(1,100);
+Console.WriteLine (numberD);
+int numberE = new Random().Next(1,100);
+Console.WriteLine (numberE);
+int max = numberA;
+
+if(numberA>max) max = numberA;
+if(numberB>max) max = numberB;
+if(numberC>max) max = numberC;
+if(numberD>max) max = numberD;
+if(numberE>max) max = numberE;
+
+Console.Write("Максимальное число -  ");
+Console.WriteLine(max);
